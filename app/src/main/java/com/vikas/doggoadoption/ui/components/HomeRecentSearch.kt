053d2892d7eed9259ce19f6fce9b09e2e1b2c116
vikas kumar <vkbookworm@gmail.com>
2021-02-28 14:43:10 +0530

    feat: added the bookmarked icon

--- app/src/main/java/com/vikas/doggoadoption/ui/components/HomeRecentSearch.kt
+++ app/src/main/java/com/vikas/doggoadoption/ui/components/HomeRecentSearch.kt
@@ -1,19 +1,15 @@
 package com.vikas.doggoadoption.ui.components
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Surface
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,15 +17,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vikas.doggoadoption.R
 import com.vikas.doggoadoption.ui.theme.DoggoAdoptionTheme
+import com.vikas.doggoadoption.ui.theme.Teal200
 import com.vikas.doggoadoption.ui.theme.Typography
 import com.vikas.doggoadoption.ui.theme.iconTintColor
 
@@ -62,6 +57,9 @@ fun HomeRecentSearch() {
 
 @Composable
 fun SearchItems() {
+
+    var bookMarked by remember { mutableStateOf(false) }
+
     Card(
         elevation = 2.dp,
         shape = RoundedCornerShape(20.dp),
@@ -126,6 +124,27 @@ fun SearchItems() {
                     maxLines = 1
                 )
             }
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            val bookMarkColor =
+                animateColorAsState(
+                    targetValue = if (bookMarked) Teal200 else iconTintColor.copy(
+                        alpha = .6f
+                    )
+                )
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_bookmark),
+                contentDescription = "",
+                tint = bookMarkColor.value,
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .size(20.dp)
+                    .clickable {
+                        bookMarked = bookMarked.not()
+                    }
+            )
         }
     }
 }
